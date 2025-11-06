37098356d5875360b2720860c6dca3b7671c1d63
Nilton Condori <a20220229@pucp.edu.pe>
2025-11-06 09:56:01 -0500

    Restaurar carpeta cliente_web eliminada accidentalmente

---
+++ cliente_web/src/main/java/com/example/cliente_web/ClienteWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.cliente_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteWebApplication.class, args);
+    }
+
+}
---
+++ cliente_web/src/test/java/com/example/cliente_web/ClienteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cliente_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
